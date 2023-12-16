cf0dee6b9998ff7d22b9c0cb7adfcf23dd9c3097
Paper99 <1398872296@qq.com>
2023-12-16 17:32:06 +0800

    1.1 发送同步到群的消息,要带上玩家UUID和名字.

--- src/main/java/cn/paper_card/qq_group_chat_sync/api/QqGroupMessageSender.java
+++ src/main/java/cn/paper_card/qq_group_chat_sync/api/QqGroupMessageSender.java
@@ -2,7 +2,9 @@ package cn.paper_card.qq_group_chat_sync.api;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 @SuppressWarnings("unused")
 public interface QqGroupMessageSender {
-    void sendNormal(@NotNull String content) throws Exception;
+    void sendNormal(@NotNull UUID uuid, @NotNull String name, @NotNull String content) throws Exception;
 }
